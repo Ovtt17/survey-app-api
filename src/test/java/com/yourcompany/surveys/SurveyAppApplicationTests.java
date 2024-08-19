c9b31820b4a7a4c1caa66c381f246c993be5432f
Ovett Mora <ovettmora17@gmail.com>
2024-08-18 20:04:32 -0600

    :zap: Initial commit

---
+++ src/test/java/com/yourcompany/surveys/SurveyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcompany.surveys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
