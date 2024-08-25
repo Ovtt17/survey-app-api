815f504b90ef21ba407f614d1c59d50abcdc8d10
Ovett Mora <ovettmora17@gmail.com>
2024-08-24 20:24:08 -0600

    feature: create Rating entity

---
+++ src/main/java/com/yourcompany/surveys/entity/Rating.java
@@ -0,0 +1,34 @@
+package com.yourcompany.surveys.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ratings")
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Double rating;
+
+    @ManyToOne
+    @JoinColumn(name = "survey_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "survey_id", nullable = false)
+    private Survey survey;
+
+}
