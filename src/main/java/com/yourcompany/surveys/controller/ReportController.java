5a2f82837e120b03e65de324dccf016f609ddbdf
Ovett Mora <ovettmora17@gmail.com>
2024-08-25 23:11:38 -0600

    refactor: change file name

--- src/main/java/com/yourcompany/surveys/controller/ReportController.java
+++ src/main/java/com/yourcompany/surveys/controller/ReportController.java
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/reports")
 @RequiredArgsConstructor
 @Tag(name = "Reports")
-public class ExportController {
+public class ReportController {
 }
