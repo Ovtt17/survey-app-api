7bb9815b1fbbc81310c5846b03729f09355ed31b
Ovett Mora <ovettmora17@gmail.com>
2024-08-27 19:19:04 -0600

    refactor: Update SurveyRepository to include query for finding popular surveys

--- src/main/java/com/yourcompany/surveys/repository/SurveyRepository.java
+++ src/main/java/com/yourcompany/surveys/repository/SurveyRepository.java
@@ -1,8 +1,10 @@
 package com.yourcompany.surveys.repository;
 
+import com.yourcompany.surveys.dto.report.PopularSurveyReportResponse;
 import com.yourcompany.surveys.entity.Survey;
 import com.yourcompany.surveys.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,13 @@ import java.util.List;
 @Repository
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
     List<Survey> findByCreator(User creator);
+
+    @Query("SELECT new com.yourcompany.surveys.dto.report.PopularSurveyReportResponse(" +
+            "s.id, s.title, COUNT(p.id)) " +
+            "FROM Survey s " +
+            "JOIN s.participations p " +
+            "GROUP BY s.id, s.title " +
+            "ORDER BY COUNT(p.id) DESC"
+    )
+    List<PopularSurveyReportResponse> findPopularSurveys();
 }
