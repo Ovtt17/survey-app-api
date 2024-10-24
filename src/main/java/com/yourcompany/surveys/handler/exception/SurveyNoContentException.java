c3bdc033ec46a46656a6f0513edb1fded61792f9
Ovett Mora <ovettmora17@gmail.com>
2024-10-23 19:19:00 -0600

    feat: Add SurveyNoContentException

---
+++ src/main/java/com/yourcompany/surveys/handler/exception/SurveyNoContentException.java
@@ -0,0 +1,7 @@
+package com.yourcompany.surveys.handler.exception;
+
+public class SurveyNoContentException extends RuntimeException {
+    public SurveyNoContentException(String message) {
+        super(message);
+    }
+}
