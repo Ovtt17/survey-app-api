f80d6dc5e83eac077f1bdacb3fa065d43d78fb09
Ovett Mora <ovettmora17@gmail.com>
2025-06-24 23:32:30 -0600

    feat: Add AuthProvider enum for authentication providers

---
+++ src/main/java/com/yourcompany/surveys/entity/AuthProvider.java
@@ -0,0 +1,23 @@
+package com.yourcompany.surveys.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthProvider {
+    LOCAL("local"),
+    GOOGLE("google"),
+    FACEBOOK("facebook");
+
+    private final String providerId;
+
+    public static Optional<AuthProvider> fromProviderId(String providerId) {
+        return Arrays.stream(values())
+                .filter(p -> p.providerId.equalsIgnoreCase(providerId))
+                .findFirst();
+    }
+}
