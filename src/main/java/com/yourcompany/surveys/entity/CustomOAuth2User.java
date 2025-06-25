b6f906fa64d74fa989e774fa4516b2a644955244
Ovett Mora <ovettmora17@gmail.com>
2025-06-24 23:32:39 -0600

    feat: Implement CustomOAuth2User for OAuth2 authentication

---
+++ src/main/java/com/yourcompany/surveys/entity/CustomOAuth2User.java
@@ -0,0 +1,30 @@
+package com.yourcompany.surveys.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+public record CustomOAuth2User(User user, Map<String, Object> attributes) implements OAuth2User {
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return OAuth2User.super.getAttribute(name);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getAuthorities();
+    }
+
+    @Override
+    public String getName() {
+        return user.getName();
+    }
+}
